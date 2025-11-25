c05edc1857f3b49be6f2c05304cea38b6872a26e
ahmedali8096 <mirzahmed8096@gmail.com>
2025-11-25 20:19:02 +0500

    Add b.cpp with error analysis and sample code
    Added b.cpp containing a sample C++ program with a common if-else error, along with comments explaining the error and its cause.

---
+++ Question-1/b.cpp
@@ -0,0 +1,20 @@
+//  Find errors
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1 = 11, num2 = 10;
+
+    if (num1 < num2)
+        cout << "num1 is the smallest";
+        num1 = num2;     
+    else
+      cout << "num2 is smallest";
+    return 0;
+}
+
+// Error: 
+// b.cpp:11:5: error: 'else' without a previous 'if' else
+//  it means if is not connected with 'else' because of no "{}" with if
+// 'if' should have curly braces '{}'
+// Output: none
