0550727a484666929d379c7fe749760d23ee9c43
ahmedali8096 <mirzahmed8096@gmail.com>
2025-11-25 19:49:30 +0500

    Add solution for Question 1 with source and binary
    Added a.cpp containing the source code and a.exe as the compiled binary for Question 1. The code demonstrates a for loop with a condition that prevents execution, resulting in only 'end of program' being printed.

---
+++ Question-1/a.cpp
@@ -0,0 +1,16 @@
+//  Find errors
+#include <iostream>
+using namespace std;
+int main()
+{
+    int I, j;
+
+    for (I = 1; I >= 7; I++)
+    cout << I;
+
+    cout << "end of program";
+    return 7;
+}
+
+// Error: No Error
+// Output: end of program
